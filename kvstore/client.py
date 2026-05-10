1cb5d98f98ee0dd49425f0eedc7024bde25850f5
Temiloluwa Ojo <engineeringwithtemi@gmail.com>
2026-05-10 11:51:40 +0100

    A3: setup multiclient and multiserver using event loop logic

--- kvstore/client.py
+++ kvstore/client.py
@@ -1,12 +1,69 @@
 import socket
+import types
+import selectors
+from typing import cast 
 
-def socketClient(hostname, port):
-  tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  tcp_socket.connect((hostname, port))
-  print("Connected to socket: ", hostname, ":", port)
+sel = selectors.DefaultSelector()
+HOST = "127.0.0.1"
+PORT = 65432
+messages = [b"Message 1 from client", b"Message 2 from client"]
+
+def create_connections(host=HOST, port=PORT, num_of_con=10):
+  for i in range(num_of_con):
+    connid = i + 1
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex((host, port))
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    data = types.SimpleNamespace(connid=connid, 
+          total_length=sum(len(m) for m in messages),
+          outb=b"", messages = messages.copy(), recv_total=0)
+    sel.register(sock, events, data=data)
+    print(f"Registered connid={connid!r}.")
 
 
 
-def closeSocket(socket: socket):
-  print("Closing socket")
-  socket.close()
+
+def service_request(key, mask):
+  sock = cast(socket.socket, key.fileobj)
+  data = cast(types.SimpleNamespace, key.data)
+  if mask & selectors.EVENT_READ:
+    recv_data = sock.recv(1024)
+    if recv_data:
+      print(f"Received {recv_data!r} from connection {data.connid}")
+      data.recv_total += len(recv_data)
+    if not recv_data or data.recv_total == data.total_length:
+      print(f"Closing connection from {data.connid}")
+      sel.unregister(sock)
+      sock.close()
+  if mask & selectors.EVENT_WRITE:
+    if not data.outb and data.messages:
+      print(f"Echoing {data.outb!r} to {data.connid}")
+      data.outb = data.messages.pop()
+    if data.outb:
+      print(f"Sending {data.outb!r} to connection {data.connid}")
+      sent = sock.send(data.outb)
+      data.outb = data.outb[sent:]
+
+
+def create_conn_request():
+  try:
+    while True:
+      events = sel.select(timeout=None)
+      for key, mask in events:
+        service_request(key, mask)
+  except KeyboardInterrupt:
+    print(f"Terminating")
+  finally:
+    sel.close()
+
+
+if __name__ == "__main__":
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument("--port", type=int, default=5000)
+  # parser.add_argument("--hostname", type=str, default="localhost")
+  # args = parser.parse_args()
+
+  # createTCPSocket(args.hostname, args.port)
+  create_connections()
+  create_conn_request()
--- kvstore/server.py
+++ kvstore/server.py
@@ -1,6 +1,66 @@
 import argparse
 import socket
 import time
+import selectors
+import types
+from typing import cast
+
+sel = selectors.DefaultSelector()
+HOST = "127.0.0.1"
+PORT = 65432
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.setblocking(False)
+sock.listen()
+sel.register(sock, selectors.EVENT_READ, data=None)
+
+
+def create_event_loop():
+  try:
+    while True:
+      events = sel.select(timeout=None)
+      print(f"Received events={len(events)}")
+      for key, mask in events:
+        if key.data is None:
+          print(f"Accepting connection")
+          accept_conn(key.fileobj)
+        else:
+          print(f"Serving request")
+          service_request(key, mask)
+  except KeyboardInterrupt:
+    print("Closing event loop")
+  finally:
+    sock.close()
+    sel.close()
+
+def accept_conn(soc:socket.socket):
+  conn, addr = sock.accept()
+  print(f"Accepted connection from {addr}!")
+  conn.setblocking(False)
+  data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+  events = selectors.EVENT_READ | selectors.EVENT_WRITE
+  sel.register(conn, events, data)
+
+def service_request(key, mask):
+  sock = cast(socket.socket, key.fileobj)
+  data = cast(types.SimpleNamespace, key.data)
+  if mask & selectors.EVENT_READ:
+    recv_data = sock.recv(1024)
+    if recv_data:
+      data.outb += recv_data
+    else:
+      print("Connection to server from {data.addr} closed.")
+      sel.unregister(sock)
+      sock.close()
+  if mask & selectors.EVENT_WRITE:
+    if data.outb:
+      print(f"Echoing {data.outb!r} to {data.addr}")
+      sent = sock.send(data.outb)
+      data.outb = data.outb[sent:]
+
+
+
 
 
 def createTCPSocket(hostname, port):
@@ -23,9 +83,10 @@ def closeSocket(socket: socket):
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--port", type=int, default=5000)
-  parser.add_argument("--hostname", type=str, default="localhost")
-  args = parser.parse_args()
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument("--port", type=int, default=5000)
+  # parser.add_argument("--hostname", type=str, default="localhost")
+  # args = parser.parse_args()
 
-  createTCPSocket(args.hostname, args.port)
+  # createTCPSocket(args.hostname, args.port)
+  create_event_loop()
